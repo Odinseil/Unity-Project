d3f181a440e451b69be6892f79291e03a6999802
Odinseil <gastbria@gmail.com>
2025-03-02 01:55:45 +0100

    Modification du script de génération

--- Assets/Scripts/#cs3.cs
+++ Assets/Scripts/#cs3.cs
@@ -1,45 +1,6 @@
 using UnityEngine;
 
-public class genererGameObject : MonoBehaviour
+public class cs3 : MonoBehaviour
 {
-    public GameObject cube;
-    public GameObject box;
-    public float x = 0;
-    public float y = 0;
-    public float z = 0;
 
-    void Start()
-    {
-        for (int i = 0; i < 17; i = i + 1)
-        {
-            GameObject myObject = Instantiate(cube);
-            myObject.transform.position = new Vector3(x, y, z);
-            for (int j = 0; j < 31; j = j + 1)
-            {
-                GameObject mySecondeObject = Instantiate(cube);
-                mySecondeObject.transform.position = new Vector3(x, y, z);
-                x = x + 0.5f;
-
-            }
-            y = y + 0.5f;
-            x = 0;
-        }
-        x = 0;
-        y = 0;
-        for (int i = 0; i < 17; i = i + 1)
-        {
-            GameObject myObject = Instantiate(box);
-            myObject.transform.position = new Vector3(x, y, z);
-            for (int j = 0; j < 31; j = j + 1)
-            {
-                GameObject mySecondeObject = Instantiate(box);
-                mySecondeObject.transform.position = new Vector3(x, y, z);
-                x = x + 0.5f;
-
-            }
-            z = z - 0.5f;
-            x = 0;
-        }
-
-    }
 }
